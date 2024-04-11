799c8e078f8ce959dadc99a82906f3e2aded881a
adammmusial <musialadam83@gmail.com>
2024-04-11 14:09:44 +0800

    feat: add append() function to append new Nodes

--- linkedlist.js
+++ linkedlist.js
@@ -4,4 +4,24 @@ export default class LinkedList {
     constructor() {
       this.listHead = null;
     }
+
+    prepend(value){
+
+        let tmp = null
+        if (this.listHead != null) tmp = this.listHead
+        this.listHead = new Node(value)
+        this.listHead.nextNode = tmp
+    }
+
+    append(value) {
+        if (this.listHead == null) this.prepend(value);
+
+        else {
+          let tmp = this.listHead;
+
+          while (tmp.nextNode != null) tmp = tmp.nextNode;
+          
+          tmp.nextNode = new Node(value);
+        }
+      }
 }
